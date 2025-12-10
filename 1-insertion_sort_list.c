c0ff0af16ca5edfba9546f1b11d6e841f19f9d69
cousinfabien <cousin.fabien3@gmail.com>
2025-12-10 14:19:47 +0000

    Fix : Task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,14 +1,13 @@
 #include "sort.h"
 
 /**
-* insertion_sort_list - sorts a doubly linked list
-* in ascending order via insertion sort
-* @list: - pointer to pointer to list of elements to sort
-*/
-
+ * insertion_sort_list - sorts a doubly linked list of integers
+ * in ascending order using insertion sort
+ * @list: pointer to pointer to the head of the list
+ */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current, *next, prev;
+	listint_t *current, *next, *prev;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
@@ -19,14 +18,16 @@ void insertion_sort_list(listint_t **list)
 	{
 		next = current->next;
 
-		while (current->prev != NULL && insert->n > current->n)
+		while (current->prev != NULL && current->prev->n > current->n)
 		{
 			prev = current->prev;
 
+			/* detach current */
 			prev->next = current->next;
 			if (current->next != NULL)
 				current->next->prev = prev;
 
+			/* move current before prev */
 			current->prev = prev->prev;
 			current->next = prev;
 
@@ -38,8 +39,8 @@ void insertion_sort_list(listint_t **list)
 			prev->prev = current;
 
 			print_list(*list);
-
 		}
+
 		current = next;
 	}
 }
