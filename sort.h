9653624a41fbac4b842989d52cf30088e5df9190
cousinfabien <cousin.fabien3@gmail.com>
2025-12-12 12:46:12 +0000

    Update : Helper Functions for quick sort

--- sort.h
+++ sort.h
@@ -25,5 +25,8 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void quick_recursive(int *array, int low, int high, size_t total_size);
+int partition_lomuto(int *array, int low, int high, size_t total_size);
+
 
 #endif /* SORT_H */
